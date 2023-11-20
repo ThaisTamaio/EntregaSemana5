26a64c33811fe5500016c09ff2117641d9e94058
jimenezzz <j.jimenez21@uniandes.edu.co>
2023-11-19 20:39:54 -0500

    escenario 11 y 18

--- CypressTesting/Cypress/cypress/e2e/testv3.cy.js
+++ CypressTesting/Cypress/cypress/e2e/testv3.cy.js
@@ -183,6 +183,68 @@ describe('Ghost Post Creation and Publication', () => {
         }
     });
 
+    it('Escenario 11: Crear un Post y Programar su Publicación para una Fecha y Hora Futura', () => {
+        // Título y contenido del post
+        const title = 'New programmed post';
+        const content = 'Contenido de new programmed post';
+        const date = '2023-11-11';
+        const time = '11:11';
+    
+        // Navegar a la sección de posts
+        cy.get('a[href="#/posts/"]').click({ multiple: true });
+    
+        // Tomar un screenshot después de navegar a la sección de posts
+        cy.screenshot(`v3-esc11-Navegar-a-la-seccion-de-posts`);
+    
+        // Esperar a que la nueva página se cargue
+        cy.url().should('include', '/posts');
+    
+        // Crear un nuevo post
+        cy.get('span').contains('New post').click();
+    
+        // Tomar un screenshot después de hacer clic en "New post"
+        cy.screenshot(`v3-esc11-Clic-en-New-post`);
+    
+        // Esperar a que la nueva página se cargue
+        cy.url().should('include', '/editor/post');
+    
+        // Llenar el título y el contenido del post
+        cy.get('textarea[placeholder="Post Title"]').type(title);
+        cy.get('div.koenig-editor__editor').type(content);
+    
+        // Tomar un screenshot después de llenar el título y contenido
+        cy.screenshot(`v3-esc11-Llenar-titulo-y-contenido`);
+    
+        // Abrir la configuración del post
+        cy.get('button[class="post-settings"]').click();
+    
+        // Configurar la fecha y hora de publicación
+        cy.get('input[placeholder="YYYY-MM-DD"]').invoke('val', date);
+        cy.get('div.gh-date-time-picker-time').invoke('val', time);
+    
+        // // Hacer clic en el botón de configuración del post antes de verificar el tag
+        cy.get('button[class="close settings-menu-header-action"]').click();
+    
+        // Publicar el post
+        cy.get('span').contains('Publish').click({ force: true });
+    
+        // Tomar un screenshot después de hacer clic en "Publish"
+        cy.screenshot(`v5-esc11-Clic-en-Publish`);
+    
+        // Esperar a que aparezca el botón de confirmación
+        cy.get('div').contains('Schedule it for later').click();
+        cy.get('span').contains('Schedule').click();
+    
+        // Tomar un screenshot después de hacer clic en "Continue, final review"
+        cy.screenshot(`v5-esc11-Clic-en-Continue-final-review`);
+    
+        // Confirmar la publicación
+        cy.visit('http://localhost:3001/ghost/#/posts');
+    
+        // Verificar el contenido y el título del post
+        cy.contains(title).should('exist');
+    });
+
     it('Escenario 15: Crear Varias Páginas Estáticas', () => {
         const pages = ['page1', 'page2', 'page3'];
     
@@ -215,4 +277,34 @@ describe('Ghost Post Creation and Publication', () => {
         });
     });   
     
+    it('Escenario 18: Agregar un Miembro y Revisar su Información de Participación', () => {
+        const email = 'juan'+Math.floor(Math.random() * 1000)+'@gmail.com';
+    
+        // Navegar a la sección de miembros
+        cy.get('a[href="#/staff/"]').first().click();
+        cy.screenshot('v3-esc18-navegarSeccionMiembros');
+    
+        // Esperar a que la nueva página se cargue
+        cy.url().should('include', '/staff');
+    
+        cy.get('span').contains('Invite people').click();
+        cy.screenshot('v3-esc18-agregarManualmente');
+    
+        // Llenar el nombre y el email del miembro
+        cy.get('input[id="new-user-email"]').type(email);
+        cy.screenshot('v3-esc18-llenarNombreEmail');
+    
+        // Guardar la información del miembro
+        cy.get('span').contains('Send invitation now').click();
+        cy.screenshot('v3-esc18-guardarMiembro');
+    
+        // Esperar a que aparezca la confirmación de guardado
+        //reload page
+        cy.reload();
+        cy.screenshot('v3-esc18-confirmacionGuardado');
+    
+        // Verificar el nombre y email del miembro
+        cy.contains(email).should('exist');
+        cy.screenshot('v3-esc18-verificarInformacionMiembro');
+    });
 });
